de2f0678eccbfa1939fc97cc6f659ff2e0f055e4
Arthur <zinartinc@gmail.com>
2018-08-06 20:59:43 -0700

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hero">
+    <div>
+      <img id="instaLogo" src="../assets/instagram-icon.png" />
+      <h1 class="display-3">Welcome to club</h1>
+      <p class="lead">Enter your login</p>
+      <button type="button" class="btn btn-secondary">Give it a try</button>
+    </div>
+  </div>
+</template>
+
+<style>
+  .hero {
+    height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+  .hero .lead {
+    font-weight: 200;
+    font-size: 1.5rem;
+  }
+  #instaLogo {
+    height: 13em;
+    weight: auto;
+  }
+</style>
